e8fb8513ca4c4ca458914061d91e59ae568baee9
Admin <admin@MacBook-Pro-3.fritz.box>
2023-04-11 20:01:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+age = input ("What is your current age")
+age_as_int = int(age)
+
+years_left = 90 - age_as_int
+days_left = 365 * years_left
+weeks_left = 52 * years_left
+months_left = 12 * years_left
+
+print(f"You have {days_left} days, {weeks_left} weeks, {months_left} months, and {years_left} years left")
